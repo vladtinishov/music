46d082f559d11c91fb017ba3ff30faaad1728574
Vladislav <vladislav04022001@gmail.com>
2021-03-09 17:26:03 +0600

    application/views/client

--- application/views/myindex.php
+++ application/views/myindex.php
@@ -7,14 +7,6 @@
 	<title>Hello, world!</title>
 </head>
 <body>
-	<?php foreach ($users as $news_item): ?>
-
-	<h2><?php echo $news_item['user_id'] ?></h2>
-	<div id="main">
-		<?php echo $news_item['user_name'] ?>
-	</div>
-	<p><a href="news/<?php echo $news_item['slug'] ?>">View article</a></p>
-
-	<?php endforeach ?>
+	
 </body>
 </html>
