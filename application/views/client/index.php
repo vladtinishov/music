39971bd64763285c5cee3193ba53487885f01bfa
Vladislav <vladislav04022001@gmail.com>
2021-03-09 18:45:17 +0600

    подключил скрипт

---
+++ application/views/client/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music</title>
+</head>
+<body>
+    <h1>Main/Music</h1>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script></script>
+<script src="<?php echo base_url();?>templates/script.js"></script>
+</body>
+</html>
